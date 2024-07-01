b67b24c68003f85e2866898146a98dcd32ca1650
shahafseg <shahafseg@gmail.com>
2024-07-01 18:12:44 +0300

    done private from public

--- app-infra/modules/eks/eks.tf
+++ app-infra/modules/eks/eks.tf
@@ -23,3 +23,17 @@ resource "aws_eks_addon" "aws-ebs-csi-driver" {
 data "aws_eks_cluster_auth" "main" {
   name = var.cluster_name
 }
+
+resource "kubernetes_storage_class" "ebs-class" {
+  metadata {
+    name = "gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  volume_binding_mode = "WaitForFirstConsumer"
+  parameters = {
+    "type" = "gp3"
+  }
+}
